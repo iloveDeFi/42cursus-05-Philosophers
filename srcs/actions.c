e566dbc79faeb6121e4eb94d6dab1f809ec1279f
Baptiste Bessard <bat@wl4s-84-168.unil.ch>
2023-07-11 16:49:44 +0200

    adding state and simulation

--- srcs/actions.c
+++ srcs/actions.c
@@ -6,25 +6,93 @@
 /*   By: bat <bat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 10:03:10 by bat               #+#    #+#             */
-/*   Updated: 2023/07/11 09:24:53 by bat              ###   ########.fr       */
+/*   Updated: 2023/07/11 15:50:09 by bat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void    eats(t_config *config)
+/* philosopher is eating */
+void	eats(t_config *config)
 {
+	int	start;
 
+	if (!state(config))
+		return ;
+	while (config->life && pthread_mutex_lock(&config->forks[config->pos]))
+		state(config);
+	msg(config, FORKS);
+	if (config->nb_philo == 1)
+		solo(config);
+	if (!config->life)
+		return ;
+	while (config->life && pthread_mutex_lock(&config->forks[config->next_pos]))
+		state(config);
+	msg(config, FORKS);
+	start = get_time('n');
+	msg(config, EATS);
+	config->last_bite = get_time('n');
+	while (config->life && (start + config->t_eat) > get_time('n'))
+		state(config);
+	pthread_mutex_unlock(&config->forks[config->pos]);
+	pthread_mutex_unlock(&config->forks[config->next_pos]);
+	if (config->life && config->nb_t_eat >= 0 \
+	&& (++config->ate == config->nb_t_eat))
+		state_update(config, 'E');
 }
 
-void    sleeps(t_config *config)
+/* philosopher is sleeping */
+void	sleeps(t_config *config)
 {
+	int	start;
 
+	state(config);
+	if (!config->life)
+		return ;
+	start = get_time('n');
+	msg(config, SLEEPS);
+	while (config->life && (start + config->t_sleep) > get_time('n'))
+		state(config);
 }
 
-void    thinks(t_config *config)
+/* philosopher is thinking */
+void	thinks(t_config *config)
 {
+	state(config);
+	if (!config->life)
+		return ;
+	msg(config, THINKS);
+}
 
+/* updates the sim_state variable based on the flag
+	if flag == 'L', put sim_state[0] == 0
+	if flag == 'E', increases sim_state[1]++
+	and check if the philos have all eaten */
+void	state_update(t_config *config, char flag)
+{
+	if (flag == 'L')
+	{
+		pthread_mutex_lock(&config->sim_mutex[0]);
+		config->sim_state[0] = 0;
+		pthread_mutex_unlock(&config->sim_mutex[0]);
+		config->life = 0;
+		msg(config, DIES);
+		return ;
+	}
+	if (flag == 'E')
+	{
+		pthread_mutex_lock(&config->sim_mutex[1]);
+		config->sim_state[1] += 1;
+		if (config->sim_state[1] == config->nb_philo)
+		{
+			pthread_mutex_lock(&config->sim_mutex[0]);
+			config->sim_state[0] = 0;
+			pthread_mutex_unlock(&config->sim_mutex[0]);
+			config->life = 0;
+		}
+		pthread_mutex_unlock(&config->sim_mutex[1]);
+	}
+	return ;
 }
 
 void	solo(t_config *config)
--- srcs/simulation.c
+++ srcs/simulation.c
@@ -6,7 +6,7 @@
 /*   By: bat <bat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 10:01:38 by bat               #+#    #+#             */
-/*   Updated: 2023/07/11 09:24:53 by bat              ###   ########.fr       */
+/*   Updated: 2023/07/11 15:50:12 by bat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void    launch_simulation(t_philo **philo)
     
     i = -1;
     create_philo(*philo);
-    while(++i < nb_philo)
+    while(++i < (*philo)->nb_philo)
+        pthread_join((*philo)->threads[i], NULL);
 }
 
 /* creates each thread of each individual philo */
@@ -29,4 +30,63 @@ void	create_philo(t_philo *philo)
 	i = -1;
 	while (++i < philo->nb_philo)
 		create_thread(philo, i);
+}
+
+/* creates a thread for a philo and sets up the config struct */
+void	create_thread(t_philo *philo, int nb_philo)
+{
+	t_config	*config;
+
+	config = malloc(sizeof(t_config));
+	if (!config)
+		return ;
+	config->pos = nb_philo;
+	if (config->pos == philo->nb_philo - 1)
+		config->next_pos = 0;
+	else
+		config->next_pos = nb_philo + 1;
+	config->t_die = philo->time_die;
+	config->t_eat = philo->time_eat;
+	config->t_sleep = philo->time_sleep;
+	config->life = 1;
+	config->ate = 0;
+	config->last_bite = get_time('n');
+	config->sim_state = philo->sim_state;
+	config->nb_philo = philo->nb_philo;
+	config->forks = philo->forks;
+	config->sim_mutex = philo->sim_mutex;
+	config->nb_t_eat = philo->nb_time_eat;
+	if (!pthread_create(&philo->threads[nb_philo], NULL, \
+	philo_day, (void *) config))
+		return ;
+}
+
+/* the daily rountine of a philo */
+void	*philo_day(void	*arg)
+{
+	t_config	*config;
+
+	config = (t_config *)arg;
+	if (config->pos % 2 != 0)
+		usleep(3000);
+	while (config->life)
+	{
+		eats(config);
+		sleeps(config);
+		thinks(config);
+	}
+	free(config);
+	return (0);
+}
+
+/* checks if all the philo have eaten enough */
+int	satiated(t_config *config)
+{
+	if (config->nb_t_eat == -1)
+		return (0);
+	pthread_mutex_lock(&config->sim_mutex[1]);
+	if (config->sim_state[1] == config->nb_philo)
+		return (1);
+	pthread_mutex_unlock(&config->sim_mutex[1]);
+	return (0);
 }
